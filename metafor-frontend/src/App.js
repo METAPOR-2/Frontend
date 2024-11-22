8f74895aab68c3756e0dd1dd6a06123779f73361
blackcat417 <ssoo174@ewha.ac.kr>
2024-11-22 23:27:51 +0900

    button

--- metafor-frontend/src/App.js
+++ metafor-frontend/src/App.js
@@ -1,5 +1,10 @@
-import { Logo } from "../src/assets/images/Logo.svg";
+import Button from "./components/button";
+
 function App() {
+  const handleLogin = () => {
+    alert("로그인 버튼이 클릭되었습니다!");
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       {/* 모바일 화면을 감싸는 컨테이너 */}
@@ -7,13 +12,11 @@ function App() {
         <header className="w-full py-4 bg-black text-white text-center">
           <h1 className="text-xl font-bold">MOISO</h1>
         </header>
-        <main className="flex-grow w-full px-4">
-          <section className="bg-white p-4 rounded-lg shadow">
-            <h2 className="text-lg font-semibold mb-2">
-              모바일 앱 화면입니다.
-            </h2>
-            <p className="text-gray-700">되는 것이겠죠?</p>
-          </section>
+        <main className="flex-grow w-full p-4">
+          <h2 className="text-lg font-semibold mb-2">모바일 앱 화면입니다.</h2>
+          <p className="text-gray-700">되는 것이겠죠?</p>
+
+          <Button onClick={handleLogin}>로그인</Button>
         </main>
         <footer className="w-full py-4 bg-gray-800 text-white text-center">
           <p className="text-sm">© 2024 MEDILUX x TAVE Hackaton Team 2</p>
