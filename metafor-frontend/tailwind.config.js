af2cd3629217db19cb62928a6fd6e281fc589800
Yeong-si <dtd1110@gmail.com>
2024-11-22 21:57:54 +0900

    setting

---
+++ metafor-frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      width: {
+        app: "100vw",
+      },
+      height: {
+        app: "100vh",
+      },
+    },
+  },
+  plugins: [],
+};
