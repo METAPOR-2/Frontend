590d87ff2f01b0564bbf81410f2f285c14779334
blackcat417 <ssoo174@ewha.ac.kr>
2024-11-23 04:07:32 +0900

    final

--- metafor-frontend/src/pages/doctorHome.jsx
+++ metafor-frontend/src/pages/doctorHome.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AiIcon from "../assets/icons/AiIcon.svg";
+import Location from "../assets/icons/locationIcon.svg";
 import Logo from "../assets/images/Logo.svg";
 import NavBar from "../components/NavBar";
 
@@ -13,11 +14,11 @@ export default function DoctorHome() {
         </header>
 
         {/* 일정관리 섹션 */}
-        <section className="px-6">
+        <section className="px-6 mb-10">
           <h2 className="text-[22px] font-semibold mb-[18px]">일정관리</h2>
 
           {/* AI 루틴 */}
-          <div className="flex items-center justify-between bg-white border border-[#3F35FF] rounded-lg p-3 mb-3">
+          <div className="flex items-center justify-between bg-white border border-[#3F35FF] rounded-[12px] p-3 mb-4">
             <div className="flex items-center gap-3">
               <img src={AiIcon} className="w-[20px]" />
               <p className="text-[#3F35FF] font-semibold text-[18px]">
@@ -25,55 +26,80 @@ export default function DoctorHome() {
               </p>
             </div>
           </div>
-          <p className="text-[12px] font-normal text-gray-400 text-right mb-3">
+          <p className="text-[12px] font-normal text-gray-400 text-right mb-4">
             Ai를 활용하여 효율적으로 고객을 관리하세요!
           </p>
 
           {/* 신청 목록 */}
-          <div className="bg-[#F9F9FF] rounded-lg p-4 mb-4">
-            <p className="text-sm text-[#1B12C7] mb-2">11월 23일 토요일</p>
-            <h3 className="text-lg font-semibold">{`{username}`}님의 신청</h3>
+          <div className="bg-[#F9F9FF] rounded-lg p-4 mb-2 w-[334px] h-[135px]">
+            <div className="flex justify-between">
+              <p className="text-sm text-[#1B12C7] mb-2">11월 21일 토요일</p>
+              <img src={Location} className="w-[15px] h-[15px]"></img>
+              {/* <p className="text-sm text-[#1B12C7] mb-2">dddd</p> */}
+            </div>
+            <h3 className="text-[20px] font-semibold mb-2">
+              {`{username}`}님의 신청
+            </h3>
             <div className="flex gap-2">
-              <button className="text-[#4A58E2] border border-[#4A58E2] rounded-full px-4 py-1 text-sm">
+              <button className="text-black bg-white border border-[#4A58E2] rounded-full w-[63px] h-[26px] px-2 text-[11px] mr-[16px]">
                 치료이름
               </button>
-              <button className="text-[#4A58E2] border border-[#4A58E2] rounded-full px-4 py-1 text-sm">
+              <button className="text-black border bg-white border-[#4A58E2] rounded-full w-[63px] h-[26px] px-2 text-[11px]">
                 치료이름
               </button>
             </div>
+            <p className="text-[12px] text-gray-400 font-normal text-right">
+              자세히 보기
+            </p>
           </div>
         </section>
 
         {/* 신청관리 섹션 */}
-        <section className="px-4">
-          <h2 className="text-xl font-bold mb-4 flex items-center justify-between">
-            신청관리{" "}
-            <span className="bg-[#4A58E2] text-white text-xs px-2 py-1 rounded-full">
-              3
-            </span>
-          </h2>
+        <section className="px-6 mb-5">
+          <div className="flex justify-between items-center">
+            <h2 className="text-[22px] font-semibold mb-4 flex items-center">
+              신청관리{" "}
+              <span className="bg-[#3F35FF] text-white font-extrabold text-[11px] px-2 py-1 ml-2 rounded-full">
+                3
+              </span>
+            </h2>
+            <p className="text-[12px] text-gray-400 font-normal mr-2">더보기</p>
+          </div>
 
           {/* 신청 카드 */}
-          {[...Array(2)].map((_, index) => (
-            <div
-              key={index}
-              className="bg-[#F7F8FF] rounded-lg p-4 mb-4 flex justify-between items-center"
-            >
-              <div>
-                <p className="text-sm text-gray-500 mb-2">11월 23일 토요일</p>
-                <h3 className="text-lg font-semibold">
-                  {`{username}`}님의 신청
-                </h3>
-                <p className="text-sm text-gray-500">{`{location}`}</p>
-              </div>
-              <button className="text-[#4A58E2]">자세히 보기</button>
+          <div className="bg-white border border-[#D0D4FF] rounded-[12px] p-3 mb-4 w-[334px] h-[68px]">
+            <div className="flex justify-between">
+              <p className="text-[12px] text-[#1B12C7]">11월 28일 토요일</p>
+              <img src={Location} className="w-[15px] h-[15px]"></img>
             </div>
-          ))}
+            <div className="flex justify-between items-center">
+              <p className="text-[18px] text-[#1B12C7] font-semibold">
+                {`{username}`}님의 신청
+              </p>
+              <p className="text-[12px] text-gray-400 font-normal">
+                자세히 보기
+              </p>
+            </div>
+          </div>
+          <div className="bg-white border border-[#D0D4FF] rounded-[12px] p-3 mb-4 w-[334px] h-[68px]">
+            <div className="flex justify-between">
+              <p className="text-[12px] text-[#1B12C7]">12월 05일 토요일</p>
+              <img src={Location} className="w-[15px] h-[15px]"></img>
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="text-[18px] text-[#1B12C7] font-semibold">
+                {`{username}`}님의 신청
+              </p>
+              <p className="text-[12px] text-gray-400 font-normal">
+                자세히 보기
+              </p>
+            </div>
+          </div>
         </section>
 
         {/* Q&A 섹션 */}
-        <section className="px-4">
-          <h2 className="text-xl font-bold">Q&A</h2>
+        <section className="px-6">
+          <h2 className="text-[22px] font-semibold">Q&A</h2>
         </section>
 
         {/* 하단 네비게이션 바 */}
