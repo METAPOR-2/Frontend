3a66ba248d670b53f89156274232aa6ee82b412e
blackcat417 <ssoo174@ewha.ac.kr>
2024-11-23 02:39:52 +0900

    final

--- metafor-frontend/src/components/input.jsx
+++ metafor-frontend/src/components/input.jsx
@@ -1,22 +1,19 @@
-import { React } from "react";
+import React from "react";
 
-/*
- textSize = 텍스트 크기,
-  placeholder = 입력 전 힌트 값,
-  
-   */
-export default function Input({
-  placeholder = "",
-  type = "text",
-  onChange = () => {},
-}) {
+export default function Input({ placeholder = "", type = "text", Icon }) {
   return (
-    <div>
+    <div className="relative w-[334px] h-[56px]">
+      {/* 아이콘 위치 */}
+      {Icon && (
+        <span className="absolute left-4 top-1/2 transform -translate-y-1/2">
+          <img src={Icon} alt="icon" className="w-5 h-5 text-gray-400" />
+        </span>
+      )}
+      {/* 입력 필드 */}
       <input
-        className="w-[334px] h-[56px] bg-gray-50 border-none rounded-[12px] focus:outline-none p-4"
+        className="w-full h-full pl-12 pr-4 bg-gray-50 border-none rounded-[12px] focus:outline-none placeholder-gray-500 text-[16px]"
         type={type}
         placeholder={placeholder}
-        onChange={onChange}
       />
     </div>
   );
