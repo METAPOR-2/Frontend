15fea2b9a0535dfd57ab76c4a53eeb86650b7e5e
blackcat417 <ssoo174@ewha.ac.kr>
2024-11-23 07:23:38 +0900

    navbar

--- metafor-frontend/src/components/NavBar.jsx
+++ metafor-frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom"; // React Router 사용
-import CalendarIcon from "../assets/icons/calendarIcon.svg";
 import ChatIcon from "../assets/icons/ChatIcon.svg";
+import CalendarIcon from "../assets/icons/calendarIcon.svg";
 import HomeIcon from "../assets/icons/homeIcon.svg";
 import myPageIcon from "../assets/icons/myPageIcon.svg";
 
@@ -10,7 +10,10 @@ export default function NavBar({ num = 1 }) {
     <nav className="fixed bottom-0 w-full max-w-[390px] bg-white border-t border-gray-300 shadow-md flex justify-between items-center p-10 h-[90px]">
       {/* Home 아이콘 */}
 
-      <Link to="/home" className="flex flex-col items-center text-gray-500">
+      <Link
+        to="/doctor-home"
+        className="flex flex-col items-center text-gray-500"
+      >
         <div
           className="w-8 h-8"
           style={{
